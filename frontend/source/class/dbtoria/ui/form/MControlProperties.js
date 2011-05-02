e12214651b8134c39f41a31b0e98239c7c17fa9f
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2011-05-02 07:31:58 +0200

    Added missing Mixin to git.

---
+++ frontend/source/class/dbtoria/ui/form/MControlProperties.js
@@ -0,0 +1,23 @@
+/* ************************************************************************
+   Copyright: 2009 OETIKER+PARTNER AG
+   License:   GPLv3 or later
+   Authors:   Tobi Oetiker <tobi@oetiker.ch>
+              Fritz Zaucker <fritz.zaucker@oetiker.ch?
+   Utf8Check: äöü
+************************************************************************ */
+
+/* ************************************************************************
+************************************************************************ */
+
+qx.Mixin.define("dbtoria.ui.form.MControlProperties", {
+
+    properties : {
+        copyForward : {
+            init     : false,
+            check    : "Boolean",
+//            apply    : '_applyCopyForward', // for debugging
+            nullable : false
+        }
+    }
+
+});
