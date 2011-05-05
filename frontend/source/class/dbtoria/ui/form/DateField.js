ae3f8ab2fbced4455583c5608303f82770e82da9
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2011-05-05 15:45:47 +0200

    Updated TODO. Experimented with fixing date saving issue.

--- frontend/source/class/dbtoria/ui/form/DateField.js
+++ frontend/source/class/dbtoria/ui/form/DateField.js
@@ -40,7 +40,7 @@ qx.Class.define("dbtoria.ui.form.DateField", {
         },
 
         clear: function() {
-            this.setValue(null)  ;
+            this.setValue(null);
         },
 
         validator: function(value,control) {
@@ -55,7 +55,63 @@ qx.Class.define("dbtoria.ui.form.DateField", {
                 control.setValid(valid);
             }
             return valid;
-        }
+        },
+
+//         /**
+//          * TODOC
+//          *
+//          * @param e {Event} TODOC
+//          * @return {void}
+//          */
+//         _fireValue : function(e) {
+//             var date = this.getValue();
+//             var epoch = null;
+
+//             if (date && date.getTime) {
+//                 epoch = date.getTime() / 1000.0;
+//             }
+
+//             this.fireDataEvent('changeValue', epoch);
+// //            this._skipApplyValue++;
+// //            this.setValue(operator + ' ' + date);
+//             this.setValue(date);
+//         },
+
+
+//         /**
+//          * provide string representation of the selection "operator text field"
+//          *
+//          * @param value {var} TODOC
+//          * @param old {var} TODOC
+//          * @return {void}
+//          */
+//         _applyValue : function(value, old) {
+//             // if (this._skipApplyValue > 0) {
+//             //     this._skipApplyValue--;
+//             //     return;
+//             // }
+
+//             var epoch = value;
+
+//             if (epoch) {
+//                 this.setValue(new Date(epoch * 1000.0));
+//             } else {
+//                 date.setValue(null);
+//             }
+//         }
+
+        // getValue: function() {
+        //     var date = this.base(arguments);
+        // //     if (date == null) {
+        // //         return date;
+        // //     }
+        //     var epoch = null;
+        //     if (date && date.getTime) {
+        //          epoch = date.getTime() / 1000.0;
+        //     }
+        //     return epoch;
+        // }
+
 
     }
 
