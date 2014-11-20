89ae10bbd7d69150626168a8ad6422f2c57164b5
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2014-11-20 08:43:20 +0100

    Added missing returns at end of file

--- frontend/source/class/dbtoria/theme/Theme.js
+++ frontend/source/class/dbtoria/theme/Theme.js
@@ -10,10 +10,10 @@
 
 qx.Theme.define("dbtoria.theme.Theme", {
     meta : {
+        appearance : dbtoria.theme.Appearance,
         color      : dbtoria.theme.Color,
         decoration : dbtoria.theme.Decoration,
         font       : dbtoria.theme.Font,
-        icon       : qx.theme.icon.Tango,
-        appearance : dbtoria.theme.Appearance
+        icon       : qx.theme.icon.Tango
     }
 });
